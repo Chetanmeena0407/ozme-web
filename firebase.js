a6df0ba2bdfa6406e6dcd1bfb10a710e1ae55fc5
Chetanmeena0407 <chetan.meena0407@gmail.com>
2026-03-29 21:07:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDa4ENLuX55ylCiDPL0c8toLspGbLw5e6c",
-  authDomain: "ozme-web.firebaseapp.com",
-  databaseURL: "https://ozme-web-default-rtdb.firebaseio.com",
-  projectId: "ozme-web",
-  storageBucket: "ozme-web.firebasestorage.app",
-  messagingSenderId: "569442370502",
-  appId: "1:569442370502:web:4c7862185ce0e07154e8c7"
+  apiKey: "AIzaSyAQyHufyjSkclDtZqJCnovPox5VHY6i1RA",
+  authDomain: "mcq-app-a6f39.firebaseapp.com",
+  projectId: "mcq-app-a6f39",
+  storageBucket: "mcq-app-a6f39.firebasestorage.app",
+  messagingSenderId: "576429265830",
+  appId: "1:576429265830:web:955439d98634f3eb603567"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const auth = getAuth(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firestore database
+const db = firebase.firestore();
